a5642550876ce476625d628584af8d585db39aa1
yuqlid <yuqlid@gmail.com>
2020-07-17 13:54:42 +0900

    FreeRTOSに準拠した割り込み設定

--- Src/spi.c
+++ Src/spi.c
@@ -77,7 +77,7 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* spiHandle)
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* SPI1 interrupt Init */
-    HAL_NVIC_SetPriority(SPI1_IRQn, 1, 0);
+    HAL_NVIC_SetPriority(SPI1_IRQn, 5, 0);
     HAL_NVIC_EnableIRQ(SPI1_IRQn);
   /* USER CODE BEGIN SPI1_MspInit 1 */
 
