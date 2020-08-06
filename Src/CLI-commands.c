fac690a929ec5398a762ab6317c985c888d97f29
yuqlid <yuqlid@gmail.com>
2020-08-07 00:49:58 +0900

    入力した文字から数字の生成，これで回転数を送る処理とか組めそう

--- Src/CLI-commands.c
+++ Src/CLI-commands.c
@@ -9,7 +9,11 @@
 
 #include "MeasurementBoard_v1.h"
 
+#include <stdio.h>
+
 extern osThreadId rs485TransmitTaskHandle;
+
+long value;
 
 static BaseType_t prvPS485PeriodicCommand( char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString )
 {
@@ -41,6 +45,64 @@ static BaseType_t prvPS485PeriodicCommand( char *pcWriteBuffer, size_t xWriteBuf
 	return xReturn;
 }
 
+static BaseType_t prvSetTargetSpeedCommand( char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString )
+{
+	const char *pcParameter;
+	BaseType_t xParameterStringLength;
+	BaseType_t xReturn;
+	static UBaseType_t uxParameterNumber = 0;
+
+	/* Remove compile time warnings about unused parameters, and check the
+	write buffer is not NULL.  NOTE - for simplicity, this example assumes the
+	write buffer length is adequate, so does not check for buffer overflows. */
+	( void ) pcCommandString;
+	( void ) xWriteBufferLen;
+	configASSERT( pcWriteBuffer );
+    
+	if(uxParameterNumber == 0){
+		sprintf( pcWriteBuffer, "New Target Speed :" );
+		uxParameterNumber = 1;
+		xReturn = pdPASS;
+	}else{
+		/* Obtain the parameter string. */
+		pcParameter = FreeRTOS_CLIGetParameter(
+							pcCommandString,		/* The command string itself. */
+							uxParameterNumber,		/* Return the next parameter. */
+							&xParameterStringLength	/* Store the parameter string length. */
+		);
+
+		if(pcParameter != NULL){
+			/* Return the parameter string. */
+			char data[] = " rpm";
+			memset( pcWriteBuffer, 0x00, xWriteBufferLen );
+			//sprintf( pcWriteBuffer, "%d rpm ", ( int ) uxParameterNumber );
+			strncat( pcWriteBuffer, ( char * ) pcParameter, ( size_t ) xParameterStringLength );
+			strncat( pcWriteBuffer, ( char * ) data, strlen( data ) );
+			strncat( pcWriteBuffer, (const char *)("\r\n"), strlen( " rpm\r\n" ) );
+			
+			value = strtol(pcParameter, NULL , 10);
+
+			/* There might be more parameters to return after this one. */
+			xReturn = pdTRUE;
+			uxParameterNumber++;
+
+		}else{
+			/* No more parameters were found.  Make sure the write buffer does
+			not contain a valid string. */
+			pcWriteBuffer[ 0 ] = 0x00;
+
+			/* No more data to return. */
+			xReturn = pdFALSE;
+
+			/* Start over the next time this command is executed. */
+			uxParameterNumber = 0;
+		}
+	}
+
+	return xReturn;
+}
+
+
 static const CLI_Command_Definition_t xParameterRS485Periodic =
 {
 	"rs485_periodic",
@@ -49,8 +111,17 @@ static const CLI_Command_Definition_t xParameterRS485Periodic =
 	0 /* No parameters are expected. */
 };
 
+
+static const CLI_Command_Definition_t xParameterSetTargetSPeed =
+{
+	"speed",
+	"\r\nspeed:\r\n Set Target Speed [rpn]\r\n",
+	prvSetTargetSpeedCommand, /* The function to run. */
+	1 /* No parameters are expected. */
+};
 void vRegisterScramgleCLICommands( void )
 {
 	/* Register all the command line commands defined immediately above. */
 	FreeRTOS_CLIRegisterCommand( &xParameterRS485Periodic );
+	FreeRTOS_CLIRegisterCommand( &xParameterSetTargetSPeed );
 }
