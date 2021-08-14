b5d7d1813e43b893d684ff7c0aab6704ab64fdd5
yuqlid <yuqlid@gmail.com>
2021-08-14 18:07:04 +0900

    Readは動いたかな

--- Src/bq78350.c
+++ Src/bq78350.c
@@ -30,10 +30,15 @@ void BQ78350_WriteWord(uint8_t addr, uint16_t *pData){
 
 void BQ78350_ReadBlock(uint8_t addr, uint8_t *pData, uint16_t len){
 
-    HAL_I2C_Mem_Read(&hi2c1, BQ78350_I2C_ADDR, addr, I2C_MEMADD_SIZE_8BIT, (uint8_t *)pData, len, 1000);
+    uint8_t send_addr = addr;
+    HAL_SMBUS_Master_Transmit_IT(&hsmbus1, BQ78350_I2C_ADDR, &send_addr, 1, SMBUS_FIRST_FRAME);
+    while(HAL_SMBUS_GetState(&hsmbus1) != HAL_SMBUS_STATE_READY);
+    HAL_SMBUS_Master_Receive_IT(&hsmbus1, BQ78350_I2C_ADDR, pData, len, SMBUS_LAST_FRAME_NO_PEC);
+    while(HAL_SMBUS_GetState(&hsmbus1) != HAL_SMBUS_STATE_READY);
+
 }
 
 void BQ78350_WriteBlock(uint8_t addr, uint8_t *pData, uint16_t len){
 
-    HAL_I2C_Mem_Write(&hi2c1, BQ78350_I2C_ADDR, addr, I2C_MEMADD_SIZE_8BIT, (uint8_t *)pData, len, 1000);
+   // HAL_I2C_Mem_Write(&hi2c1, BQ78350_I2C_ADDR, addr, I2C_MEMADD_SIZE_8BIT, (uint8_t *)pData, len, 1000);
 }
